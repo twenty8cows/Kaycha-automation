f92b6854a2e9e0804715ddaad1d95522959333f5
twenty8cows <139688457+twenty8cows@users.noreply.github.com>
2023-12-20 00:24:59 -0500

    Refactored
    Applied functional programming concepts

--- add_to_local.py
+++ add_to_local.py
@@ -2,47 +2,63 @@ import os
 from openpyxl import load_workbook
 from config.confidential_info import operating_file_path_instance
 
-# # Path where the Excel file is located
-operating_file_path = operating_file_path_instance.operating_file_path
-excel_filename = operating_file_path_instance.excel_filename  # The provided Excel file
-full_excel_path = os.path.join(operating_file_path, excel_filename)
 
-# Load the existing Excel workbook
-workbook = load_workbook(full_excel_path, keep_vba=True)
-worksheet = workbook["Kaycha"]  # Access the 'Kaycha' worksheet
+def find_files_with_suffix(folder_path, suffix):
+    return [f for f in os.listdir(folder_path) if f.endswith(suffix)]
 
-# Find the first empty cell in column A
-first_empty_row = 1
-for row in worksheet.iter_rows(min_col=1, max_col=1):
-    if row[0].value is None:
-        break
-    first_empty_row += 1
 
-# Find the file in the downloads folder that ends with _processed.xlsx
-downloads_folder_path = os.path.expanduser('~/Downloads')
-matching_files = [f for f in os.listdir(
-    downloads_folder_path) if f.endswith('_processed.xlsx')]
+def load_excel_workbook(path, sheet_name=None, keep_vba=True):
+    workbook = load_workbook(path, keep_vba=keep_vba)
+    worksheet = workbook[sheet_name] if sheet_name else workbook.active
+    return workbook, worksheet
 
-if not matching_files:
-    print("No processed file found")
-    exit()  # Exit the script if no file is found
-else:
+
+def find_first_empty_row(worksheet):
+    for idx, row in enumerate(worksheet.iter_rows(min_col=1, max_col=1), start=1):
+        if row[0].value is None:
+            return idx
+    return None
+
+
+def append_data_to_worksheet(source_sheet, destination_worksheet, start_row):
+    for row in source_sheet.iter_rows(min_row=2, values_only=True):
+        for col_index, cell_value in enumerate(row, start=1):
+            destination_worksheet.cell(
+                row=start_row, column=col_index, value=cell_value)
+        start_row += 1
+
+
+def main():
+    operating_file_path = operating_file_path_instance.operating_file_path
+    excel_filename = operating_file_path_instance.excel_filename
+    full_excel_path = os.path.join(operating_file_path, excel_filename)
+
+    workbook, worksheet = load_excel_workbook(full_excel_path, "Kaycha")
+    first_empty_row = find_first_empty_row(worksheet)
+
+    downloads_folder_path = os.path.expanduser('~/Downloads')
+    processed_files = find_files_with_suffix(
+        downloads_folder_path, '_processed.xlsx')
+
+    if not processed_files:
+        print("No processed file found")
+        exit()
+
     processed_file_path = os.path.join(
-        downloads_folder_path, matching_files[0])
+        downloads_folder_path, processed_files[0])
     print(f"Found processed file: {processed_file_path}")
 
-# Load the processed workbook
-processed_workbook = load_workbook(processed_file_path)
-# Assuming data is in the active sheet
-processed_sheet = processed_workbook.active
+    # Here, if sheet_name is None, the function will return the active sheet
+    processed_workbook, processed_sheet = load_excel_workbook(
+        processed_file_path)
+    if not processed_sheet:  # If no specific sheet name is given, use the active sheet
+        processed_sheet = processed_workbook.active
 
-# Append the data from the processed sheet to the destination worksheet
-# Skip header if present
-for row in processed_sheet.iter_rows(min_row=2, values_only=True):
-    for col_index, cell_value in enumerate(row, start=1):
-        worksheet.cell(row=first_empty_row, column=col_index, value=cell_value)
-    first_empty_row += 1
+    append_data_to_worksheet(processed_sheet, worksheet, first_empty_row)
 
-# Save the updated workbook
-workbook.save(full_excel_path)
-print(f"Data appended successfully to {full_excel_path}")
+    workbook.save(full_excel_path)
+    print(f"Data appended successfully to {full_excel_path}")
+
+
+if __name__ == "__main__":
+    main()
--- work_with_export.py
+++ work_with_export.py
@@ -4,58 +4,63 @@ import os
 import sys
 from config.confidential_info import operating_file_path_instance
 
-# Define replacements dictionary
-replacements = {"NT": 0, "ND": 0, "PASS": 0}
 
-# Downloads folder path
-downloads_folder_path = os.path.join(os.path.expanduser("~"), "Downloads")
+def find_csv_files(folder_path, prefix="NA", suffix=".csv"):
+    return [f for f in os.listdir(folder_path) if f.startswith(prefix) and f.endswith(suffix)]
 
-# Find the downloaded file
-matching_files = [f for f in os.listdir(
-    downloads_folder_path) if f.startswith("NA") and f.endswith(".csv")]
 
-if not matching_files:
-    print("No matching CSV files found in Downloads folder.")
-    sys.exit()
-else:
-    # Assume the first downloaded file is the relevant one
-    downloaded_file_path = os.path.join(
-        downloads_folder_path, matching_files[0])
-
-    # Open the CSV file for processing
-    with open(downloaded_file_path, 'r') as csvfile:
+def read_csv_file(file_path):
+    with open(file_path, 'r') as csvfile:
         reader = csv.reader(csvfile)
         next(reader)  # skip header row
-        data = list(reader)
+        return list(reader)
 
-    # Replace specific values in data
-    for row_index, row in enumerate(data):
-        for col_index, cell_value in enumerate(row):
-            if cell_value in replacements:
-                data[row_index][col_index] = replacements[cell_value]
 
-    # Create new workbook and sheet
-    wb1 = Workbook()
-    ws1 = wb1.active
+def replace_values(data, replacements):
+    return [[replacements.get(cell, cell) for cell in row] for row in data]
 
-    # Copy processed data to new workbook
-    data_row = 1
-    for row in data:
-        data_col = 1
-        for cell_value in row:
-            ws1.cell(row=data_row, column=data_col).value = cell_value
-            data_col += 1
-        data_row += 1
 
-    # Generate the processed file path
-    processed_file_path = os.path.splitext(downloaded_file_path)[
-        0] + "_processed.xlsx"
+def create_workbook(data):
+    wb = Workbook()
+    ws = wb.active
+    for row_data in data:
+        ws.append(row_data)
+    return wb
 
-    # Save new workbook with dynamically generated filename
-    wb1.save(processed_file_path)
 
+def save_workbook(wb, file_path):
+    wb.save(file_path)
+
+
+def process_csv_file(csv_file_path, replacements, processed_file_suffix="_processed.xlsx"):
+    data = read_csv_file(csv_file_path)
+    processed_data = replace_values(data, replacements)
+    wb = create_workbook(processed_data)
+    processed_file_path = os.path.splitext(
+        csv_file_path)[0] + processed_file_suffix
+    save_workbook(wb, processed_file_path)
+    return processed_file_path
+
+
+def main():
+    downloads_folder_path = os.path.join(os.path.expanduser("~"), "Downloads")
+    matching_files = find_csv_files(downloads_folder_path)
+
+    if not matching_files:
+        print("No matching CSV files found in Downloads folder.")
+        sys.exit()
+
+    downloaded_file_path = os.path.join(
+        downloads_folder_path, matching_files[0])
+    replacements = {"NT": 0, "ND": 0, "PASS": 0}
+
+    processed_file_path = process_csv_file(downloaded_file_path, replacements)
     print(
         f"Data processed successfully. New workbook saved as: {processed_file_path}")
 
-os.system(operating_file_path_instance.add_to_local)
-print("Chained script add to local execution completed.")
+    os.system(operating_file_path_instance.add_to_local)
+    print("Chained script add to sp execution completed.")
+
+
+if __name__ == "__main__":
+    main()
